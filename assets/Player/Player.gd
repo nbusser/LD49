4f95476495da2785641223da2ad4c3c726927b99
Antoine Brûlé <antoine.brule@netc.eu>
2021-10-02 15:47:02 +0200

    add projectile

---
+++ assets/Player/Player.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+onready var Projectile = preload("res://assets/Projectile/Projectile.tscn")
+
+onready var world = get_parent()
+onready var projectile_origin = $cannon/projectile_origin.global_position
+
+func _ready():
+	pass
+
+func _input(event):
+	if event.is_action_pressed("shoot"):
+		shoot()
+
+func shoot():
+	var shoot_origin = projectile_origin
+	var shoot_dir = Vector2(1, -0.4)
+	var shoot_velocity = shoot_dir * 300
+	var projectile = Projectile.instance()
+	world.add_child(projectile)
+	projectile.global_transform.origin = shoot_origin
+	projectile.linear_velocity = shoot_velocity
