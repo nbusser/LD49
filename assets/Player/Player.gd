08453dac6eb5e718278b3a714d76a57e39a71bae
Antoine Brûlé <antoine.brule@netc.eu>
2021-10-02 16:27:31 +0200

    add cannon animation

--- assets/Player/Player.gd
+++ assets/Player/Player.gd
@@ -4,7 +4,11 @@ onready var Projectile = preload("res://assets/Projectile/Projectile.tscn")
 
 onready var world = get_parent()
 onready var cannon = $cannon
+onready var cannon_sprite = $cannon/cannon
 onready var projectile_origin = $cannon/projectile_origin
+
+var can_shoot = true
+const SHOOT_COOLDOWN = 0.3
 
 func _ready():
 	pass
@@ -21,3 +25,11 @@ func shoot():
 	world.add_child(projectile)
 	projectile.global_transform.origin = shoot_origin
 	projectile.linear_velocity = shoot_velocity
+	
+	animate_cannon()
+
+func animate_cannon():
+	$tween.interpolate_property(cannon_sprite, "position",
+		Vector2(-10, 0), Vector2(0, 0), SHOOT_COOLDOWN,
+		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$tween.start()
