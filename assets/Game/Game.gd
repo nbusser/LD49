e61432fb84aa19971d1a290880ec7f53a3256deb
Antoine Brûlé <antoine.brule@netc.eu>
2021-10-03 20:04:53 +0200

    fix debug sliders

--- assets/Game/Game.gd
+++ assets/Game/Game.gd
@@ -2,9 +2,9 @@ extends Node
 
 signal update_weather
 
-onready var time = $Background/time
-onready var weather = $Background/weather
-onready var shader = $Background/ColorRect.material
+onready var time = $HudLayer/time
+onready var weather = $HudLayer/weather
+onready var bg_shader = $Background/bg.material
 onready var rain = $Weather/rain
 
 var cutscene_mode = false
@@ -17,10 +17,10 @@ func _ready():
 
 
 func update_time(value):
-	shader.set_shader_param("time", value)
+	bg_shader.set_shader_param("time", value)
 
 func update_weather(value):
-	shader.set_shader_param("weather", value)
+	bg_shader.set_shader_param("weather", value)
 	rain.lifetime = 1 + (1 - value) * 10
 	rain.emitting = value > 0.0
 	emit_signal("update_weather", value)
