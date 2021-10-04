40cb824f5db117ecba90f3621bfee2e049171fb5
Matthieu Baty <matthieu.baty@inria.fr>
2021-10-04 17:15:38 +0200

    remove debug print

--- assets/Map/Wave.gd
+++ assets/Map/Wave.gd
@@ -27,7 +27,6 @@ func init(curve):
 		var prev_y = target_curve[i-1].y
 		var curr = target_curve[i]
 		var shift_y = randi()%(2*amp_y)-amp_y
-		print(prev_y)
 		if (prev_y + shift_y < 50 || prev_y + shift_y > 800):
 			shift_y = -shift_y
 		target_curve[i] = Vector2(curr.x, prev_y + shift_y)
