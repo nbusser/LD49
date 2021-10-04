6ebc580c50e44a23a3f3beaedc6f20a61cba65cc
Nicolas BUSSER <nicolas.busser67@gmail.com>
2021-10-04 19:22:38 +0200

    allow mouette spawning in both directions

--- assets/Powerup/Mouette.gd
+++ assets/Powerup/Mouette.gd
@@ -1,23 +1,57 @@
 extends Node2D
 
 # Called when the node enters the scene tree for the first time.
-const SPEED = Globals.PLAYER_MAXIMUM_SPEED + 10
+var speed
 
 onready var player = get_parent().get_node("Player")
 
 var is_dying = false
 
+enum DIRECTION {
+	LEFT_TO_RIGHT = 1
+	RIGHT_TO_LEFT = -1
+}
+
+var dir
+var destination_x
+
+func init(player, global_pos_y, direction=DIRECTION.LEFT_TO_RIGHT):
+	self.player = player
+	self.dir = direction
+
+	global_position = Vector2(
+		player.global_position.x - (Globals.buffer_size.x * direction),
+		global_pos_y
+	)
+	
+	match direction:
+		DIRECTION.LEFT_TO_RIGHT:
+			speed = Globals.PLAYER_MAXIMUM_SPEED + 10
+		DIRECTION.RIGHT_TO_LEFT:
+			speed = Globals.PLAYER_DEFAULT_SPEED
+			$Sprite.flip_h = true
+
 func _ready():
 	$SoundFx/SpawnSound.play()
 	
 func _process(delta):
-	if global_position.x > player.global_position.x + Globals.buffer_size.x:
+	var destination_x = player.global_position.x + (Globals.buffer_size.x * self.dir)
+	destination_x = player.global_position.x + (600 * self.dir)
+	if (
+		(
+			dir == DIRECTION.LEFT_TO_RIGHT
+			and global_position.x > destination_x
+		) or
+		(
+			dir == DIRECTION.RIGHT_TO_LEFT
+			and global_position.x < destination_x
+		)
+	):
 		queue_free()
 	
-	position.x += delta*SPEED
+	position.x += delta*speed*dir
 
 func _on_Hitbox_body_entered(body):
-	print("touché")
 	$SoundFx/DeathSound.play_sound()
 
 func _on_DeathSound_finished():
