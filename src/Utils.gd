930f2ed6262e355a34f71b2ebd684f00fbd5c00b
Antoine Brûlé <antoine.brule@netc.eu>
2021-10-04 23:01:48 +0200

    fix tween in Utils

--- src/Utils.gd
+++ src/Utils.gd
@@ -1,5 +1,9 @@
 extends Node
 
+var tween = Tween.new()
+
+func _ready():
+	add_child(tween)
 
 func sigmoid(x: float, k: float):
 	return 1.0 / (1.0 + exp((0.5 - x) * k))
@@ -7,15 +11,15 @@ func sigmoid(x: float, k: float):
 func fade_node_in(node, duration = 1, delay = 0):
 	if (delay > 0):
 		yield(get_tree().create_timer(delay), "timeout")
-	$Tween.interpolate_property(node, "modulate", Color(1, 1, 1, 0), Color(1, 1, 1, 1),
+	tween.interpolate_property(node, "modulate", Color(1, 1, 1, 0), Color(1, 1, 1, 1),
 		duration,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Tween.start()
+	tween.start()
 
 func fade_node_out(node, duration = 1, delay = 0):
 	if (delay > 0):
 		yield(get_tree().create_timer(delay), "timeout")
-	$Tween.interpolate_property(node, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0),
+	tween.interpolate_property(node, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0),
 		duration,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Tween.start()
+	tween.start()
